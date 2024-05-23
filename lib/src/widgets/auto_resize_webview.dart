791213d80a2cdfe5dde9d91a478c7aa4d12e8226
cdgeass <cdgeass97@gmail.com>
2024-05-23 06:34:30 +0000

    Upgarde flutter_inappwebview

--- lib/src/widgets/auto_resize_webview.dart
+++ lib/src/widgets/auto_resize_webview.dart
@@ -23,26 +23,26 @@ class _AutoResizeWebViewState extends State<AutoResizeWebView>
     super.build(context);
     var url = widget.url;
     if (url.startsWith('https://www.bilibili.com/video/')) {
-      final bvid = url
+      var id = url
           .replaceAll('https://www.bilibili.com/video/', '')
           .replaceAll('/', '')
           .split('?')[0];
-      url = 'https://player.bilibili.com/player.html?bvid=$bvid';
+      if (id.startsWith('BV')) {
+        url = 'https://player.bilibili.com/player.html?bvid=$id&autoplay=0';
+      } else {
+        id = id.replaceAll('av', '');
+        url = 'https://player.bilibili.com/player.html?aid=$id&autoplay=0';
+      }
     } else if (url.startsWith('https://youtu.be/')) {
       url = url.replaceFirst('https://youtu.be/',
           'https://www.youtube.com/embed/QdBZY2fkU-0?si=NmWFz6qgan_jSLBY');
     }
 
     if (url.startsWith('https://store.steampowered.com/widget')) {
-      _height = 73.0;
-    } else if (url.startsWith('file:///music.163.com/outchain/player')) {
-      url = url.replaceFirst('file:///', 'https://').replaceAllMapped(
-        RegExp(r'height=(\d+)'),
-        (match) {
-          return 'height=70';
-        },
-      );
-      _height = 70.0;
+      _height = 80.0;
+    } else if (url.startsWith('//music.163.com/outchain/player')) {
+      url = 'https:$url';
+      _height = 66.0;
     }
 
     if (_height == null) {
@@ -53,11 +53,16 @@ class _AutoResizeWebViewState extends State<AutoResizeWebView>
     return SizedBox(
       height: _height,
       child: InAppWebView(
-        initialUrlRequest: URLRequest(url: WebUri(url)),
+        initialData: InAppWebViewInitialData(
+          data: '''
+            <iframe src="$url" width="100%" height="100%" frameborder="0"
+              allowfullscreen="allowfullscreen" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts allow-popups"/>
+          ''',
+        ),
         initialSettings: InAppWebViewSettings(
           transparentBackground: true,
-          javaScriptEnabled: false,
-          javaScriptCanOpenWindowsAutomatically: false,
+          javaScriptEnabled: true,
+          builtInZoomControls: false,
         ),
         onLoadStop: (controller, uri) async {
           if (_height != null) {
